0caf42cd07c7928cdb8aac86567152e9998df755
sklores <sklores@gmail.com>
2025-09-02 09:17:45 -0400

    update grey background again...

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
-// src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'        // ← single global stylesheet
+import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
