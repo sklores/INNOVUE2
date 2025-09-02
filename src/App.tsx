945c4a3ff8fbdd8c193ed400e0ea889de30b34d7
sklores <sklores@gmail.com>
2025-09-02 08:15:18 -0400

    fix marquee

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import TopBarShell from './components/topbar/TopBarShell';
-import KpiTiles from './features/kpi/KpiTiles';
-import Marquee from './features/marquee/Marquee';
-import BottomBar from './components/bottombar/BottomBar';
+import TopBarShell from "./components/topbar/TopBarShell";
+import KpiTiles   from "./features/kpi/KpiTiles";
+import Marquee    from "./features/marquee/Marquee";
+import BottomBar  from "./components/bottombar/BottomBar";
 
 export default function App() {
   return (
-    <main className="page">
+    <>
       <TopBarShell />
       <KpiTiles />
       <Marquee />
       <BottomBar />
-    </main>
+    </>
   );
 }
