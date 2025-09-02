614851e3cb0095c0bc4255b0c9aa0f9bf5142cc1
sklores <sklores@gmail.com>
2025-09-02 09:10:16 -0400

    update background style

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+// src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles.css'
 import App from './App.tsx'
-import './styles/background.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
