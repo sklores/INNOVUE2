55dce4831bfc22224426f9651a0ef515b21e4dfd
sklores <sklores@gmail.com>
2025-09-02 12:30:36 -0400

    advanced vista building branch

--- src/components/topbar/ClientLogo.tsx
+++ src/components/topbar/ClientLogo.tsx
@@ -3,29 +3,41 @@ import React from "react";
 import { BADGE as B } from "./tuning";
 
 /**
- * Renders ONLY the raw GCDC image.
- * Size/position come straight from tuning.BADGE.
+ * GCDC logo sized/positioned by a wrapper div.
+ * Wrapper width/height come from tuning; the <img> just fills the wrapper.
  */
 const ClientLogo: React.FC = () => {
   return (
-    <img
-      src={B.imgSrc}
-      alt="GCDC"
+    <div
       style={{
         position: "absolute",
-        left: B.offsetLeft,  // px from left edge of the scene
-        top: B.offsetTop,    // px from top edge of the scene
-        width: B.width,      // px
-        height: B.height,    // px
-        objectFit: "contain",
-        opacity: B.opacity ?? 1,
+        left: B.offsetLeft,   // px from left edge of the scene
+        top: B.offsetTop,     // px from top edge of the scene
+        width: B.width,       // px
+        height: B.height,     // px
         pointerEvents: "none",
-        // remove any inherited transforms or max-width rules
-        transform: "none",
+        // make sure nothing else sneaks in
         maxWidth: "none",
         maxHeight: "none",
       }}
-    />
+    >
+      <img
+        src={B.imgSrc}
+        alt="GCDC"
+        style={{
+          display: "block",
+          width: "100%",
+          height: "100%",
+          objectFit: "contain",
+          opacity: B.opacity ?? 1,
+          maxWidth: "none",
+          maxHeight: "none",
+          transform: "none",
+          boxSizing: "content-box",
+          pointerEvents: "none",
+        }}
+      />
+    </div>
   );
 };
 
