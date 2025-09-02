9bd3ba3c2137f961a1abb64fec1d81a73f7c7349
sklores <sklores@gmail.com>
2025-09-02 09:12:23 -0400

    chore: use index.css as single global style

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 // src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import './styles.css'
+import './index.css'        // ← single global stylesheet
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
