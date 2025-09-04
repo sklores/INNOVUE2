a4ca510a2b7a7ffd4c49580765614c1182d47940
sklores <sklores@gmail.com>
2025-09-03 23:23:36 -0400

    bottom update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+// src/App.tsx
 import TopBarShell from "./components/topbar/TopBarShell";
 import SelectorsBar from "./components/topbar/SelectorsBar";
 import KpiTiles     from "./features/kpi/KpiTiles";
 import Marquee      from "./features/marquee/Marquee";
+import BottomSafeArea from "./components/bottombar/BottomSafeArea"; // ⬅️ new spacer
 import BottomBar    from "./components/bottombar/BottomBar";
 
 export default function App() {
@@ -11,6 +13,10 @@ export default function App() {
       <SelectorsBar />
       <KpiTiles />
       <Marquee />
+
+      {/* Spacer keeps content clear of the sticky bottom bar */}
+      <BottomSafeArea />
+
       <BottomBar />
     </>
   );
